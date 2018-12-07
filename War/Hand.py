d730cb97b529312684226889016afc6692e5f082
Matan Oded <matanoded@gmail.com>
2018-12-07 02:21:10 +0200

    infrastructure

---
+++ War/Hand.py
@@ -0,0 +1,9 @@
+import Deck
+
+
+class Hand(Deck):
+    """Represents a hand of playing cards."""
+
+    def __init__(self, label=''):
+        self.cards = []
+        self.label = label
---
+++ War/Main.py
@@ -0,0 +1,28 @@
+import Deck
+import Hand
+
+
+def find_defining_class(obj, method_name):
+    """Finds and returns the class object that will provide
+    the definition of method_name (as a string) if it is
+    invoked on obj.
+
+    obj: any python object
+    method_name: string method name
+    """
+    for ty in type(obj).mro():
+        if method_name in ty.__dict__:
+            return ty
+    return None
+
+
+if __name__ == '__main__':
+    deck = Deck()
+    deck.shuffle()
+
+    hand = Hand()
+    print find_defining_class(hand, 'shuffle')
+
+    deck.move_cards(hand, 5)
+    hand.sort()
+    print hand
